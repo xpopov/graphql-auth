3a00e7c9461324b3867f175dfee2d4b648fc1a5a
Max Popov <max.popov@gmail.com>
2020-04-04 01:51:43 +0300

    simplest way

--- lib/graphql-auth/engine.rb
+++ lib/graphql-auth/engine.rb
@@ -5,9 +5,9 @@ module GraphQL
 
       config.autoload_paths += Dir["#{config.root}/app/**/"]
 
-      initializer "graph_q_l_auth.add_middleware" do |app|
-        app.middleware.use GraphQL::Auth::Middleware
-      end
+      # initializer "graph_q_l_auth.add_middleware" do |app|
+      #   app.middleware.use GraphQL::Auth::Middleware
+      # end
     end
   end
 end

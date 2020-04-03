a920494149cdc71515f6c0187e9e8415e3a02ee6
Max Popov <max.popov@gmail.com>
2020-04-04 01:56:13 +0300

    only graphql for autoload

--- lib/graphql-auth/engine.rb
+++ lib/graphql-auth/engine.rb
@@ -3,7 +3,8 @@ module GraphQL
     class Engine < ::Rails::Engine
       # isolate_namespace GraphQL::Auth
 
-      config.autoload_paths += Dir["#{config.root}/app/**/"]
+      config.autoload_paths += Dir["#{config.root}/app/graphql"]
+      # config.autoload_paths += Dir["#{config.root}/app/**/"]
 
       # initializer "graph_q_l_auth.add_middleware" do |app|
       #   app.middleware.use GraphQL::Auth::Middleware

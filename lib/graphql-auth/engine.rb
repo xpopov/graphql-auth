16b3bfc122aa8e45a2e25c00959e5974f4e3d3df
Max Popov <max.popov@gmail.com>
2020-04-04 01:47:53 +0300

    testing approach with initializer and no isolation

--- lib/graphql-auth/engine.rb
+++ lib/graphql-auth/engine.rb
@@ -1,9 +1,13 @@
 module GraphQL
   module Auth
     class Engine < ::Rails::Engine
-      isolate_namespace GraphQL::Auth
+      # isolate_namespace GraphQL::Auth
 
       config.autoload_paths += Dir["#{config.root}/app/**/"]
+
+      initializer "graph_q_l_auth.add_middleware" do |app|
+        app.middleware.use GraphQL::Auth::Middleware
+      end
     end
   end
 end
